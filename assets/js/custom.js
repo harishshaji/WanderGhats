01d67da056809efa27c8952fe4d70292110225a9
Harish shaji <93399652+harishshaji@users.noreply.github.com>
2024-07-21 19:38:30 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,20 +9,6 @@
 
     });
 
-	// WOW JS
-	$(window).on ('load', function (){
-        if ($(".wow").length) { 
-            var wow = new WOW ({
-                boxClass:     'wow',      // Animated element css class (default is wow)
-                animateClass: 'animated', // Animation css class (default is animated)
-                offset:       20,         // Distance to the element when triggering the animation (default is 0)
-                mobile:       true,       // Trigger animations on mobile devices (default is true)
-                live:         true,       // Act on asynchronously loaded content (default is true)
-            });
-            wow.init();
-        }
-    });
-
 	$(window).scroll(function() {
 	  var scroll = $(window).scrollTop();
 	  var box = $('.header-text').height();
